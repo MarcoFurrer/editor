efbbf54f763656f55fd0a5c7948baca4d7701a97
Marco Furrer <marcofurrer0@gmail.com>
2025-07-24 13:50:38 +0200

    fix: Update build-lib script and enhance Vite configuration for library mode

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,8 @@ export default defineConfig(({ mode }) => {
               'react-dom': 'ReactDOM'
             }
           }
-        }
+        },
+        emptyOutDir: true
       }
     }
   }
